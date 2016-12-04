38c9c98ca3d170a320e773e3ce4b7f2ec46418e6
Gabriel Samson <gsamson@my.smccd.edu>
2016-12-04 13:51:35 -0800

    Adds artworkUrl field to Release type

--- src/graphql/types/Release.js
+++ src/graphql/types/Release.js
@@ -1,6 +1,8 @@
 import ReleaseTrack from './ReleaseTrack'
 import ReleaseType from './ReleaseType'
 import ReleaseStatus from './ReleaseStatus'
+import AWS from 'aws-sdk'
+const s3 = new AWS.S3()
 
 // @NOTE Release doesn't have a resolver for tracklist because tracks objects are still
 // stored in the object. We just created a separated ReleaseTrack type for organization.
@@ -15,6 +17,7 @@ const Release = `
     stripeOrderId: String,
     type: ReleaseType!,
     artworkS3Key: String,
+    artworkUrl: String,
     title: String,
     artist: String,
     recordLabel: String,
@@ -29,6 +32,13 @@ const Release = `
 `
 
 export const resolver = {
+  artworkUrl(release, args, context) {
+    if(!release.artworkS3Key)
+      return null
+    const params = { Bucket: 'jamout-distribution', Key: release.artworkS3Key }
+    const url = s3.getSignedUrl('getObject', params)
+    return url
+  },
   status(release, args, context) {
     return {
       d: 'DRAFT',
