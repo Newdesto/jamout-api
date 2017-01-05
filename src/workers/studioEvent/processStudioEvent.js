ccd453aa9eae5954f5a2ac115b89d02c479e54b5
Chris Chuck <chrischuck35@gmail.com>
2017-01-04 16:55:35 -0800

    Starts studio session worker

--- src/workers/studioEvent/processStudioEvent.js
+++ src/workers/studioEvent/processStudioEvent.js
@@ -13,13 +13,13 @@ queue.process('studio-sessions:inquire', async ({ data }, done) => {
 
   if ( date && studio && time && userId ) {
     const inquiry = await NewEvent.createStudioEvent(data.userId, 'new-inquiry', data)
+    await eventRequestAndProcess({ name: 'studio-session:inquire--complete' }, { sessionId: userId })
     done(null, inquiry)
   }
 
   if (!studio) {
     const { response, events } = await eventRequestAndProcess({ name: 'studio-session:inquire--studio' }, { sessionId: userId });
-    console.log(response)
-    console.log(events)
+
     // persist messages
     const jobs = await Promise.all(events.messages.map((message, index) => createJob('assistant.persistMessage', {
         title: `Persist assistant messages for user (${userId})`,
@@ -39,9 +39,43 @@ queue.process('studio-sessions:inquire', async ({ data }, done) => {
   }
 
   if (!date) {
+    const { response, events } = await eventRequestAndProcess({ name: 'studio-session:inquire--date' }, { sessionId: userId });
 
+    // persist messages
+    const jobs = await Promise.all(events.messages.map((message, index) => createJob('assistant.persistMessage', {
+        title: `Persist assistant messages for user (${userId})`,
+        message,
+        userId,
+      }, (index * 1000)))) // @NOTE the one second delay bc our write/sec = 1 LOL
+
+    // Publish events to the client.
+    // Priority: 1) Messages 2) Redirect 3) UI Updates
+    events.messages.forEach(message => {
+      // @NOTE we optimistically generate a timestamp and id
+      message.createdAt = new Date().toISOString()
+      message.id = uuid()
+      pubsub.publish(`assistant.${userId}`, message)
+    })
+    pubsub.publish(`assistant.${userId}`, events.input)
   }
   if (!time) {
+    const { response, events } = await eventRequestAndProcess({ name: 'studio-session:inquire--time' }, { sessionId: userId });
 
+    // persist messages
+    const jobs = await Promise.all(events.messages.map((message, index) => createJob('assistant.persistMessage', {
+        title: `Persist assistant messages for user (${userId})`,
+        message,
+        userId,
+      }, (index * 1000)))) // @NOTE the one second delay bc our write/sec = 1 LOL
+
+    // Publish events to the client.
+    // Priority: 1) Messages 2) Redirect 3) UI Updates
+    events.messages.forEach(message => {
+      // @NOTE we optimistically generate a timestamp and id
+      message.createdAt = new Date().toISOString()
+      message.id = uuid()
+      pubsub.publish(`assistant.${userId}`, message)
+    })
+    pubsub.publish(`assistant.${userId}`, events.input)
   }
 });
