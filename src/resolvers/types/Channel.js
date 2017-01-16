d177d4dfb2984f74cd6f6b3112557e03c76307db
Gabriel Samson <gsamson@my.smccd.edu>
2017-01-15 16:56:55 -0800

    Adds channel type resolver

--- src/resolvers/types/Channel.js
+++ src/resolvers/types/Channel.js
@@ -1,20 +1,14 @@
 import Message from './Message'
 import Profile from './Profile'
 
-const Channel = `
-  type Channel {
-    createdAt: String!,
-    updatedAt: String,
-    id: ID!
-    type: String!
-    name: String,
-    messages: [Message!]!,
-    ownerUserId: ID,
-    users: [Profile!]!
-  }
-`
-
-export const resolver = {
+const resolvers = {
+  type({ type }) {
+    return {
+      a: 'ASSISTANT',
+      d: 'DM',
+      g: 'GROUP'
+    }[type]
+  },
   async messages({ id }, args, { Message }) {
     return Message.getMessages(id, args.messageLimit)
   },
@@ -23,8 +17,4 @@ export const resolver = {
   }
 }
 
-export default () => [
-  Channel,
-  Message,
-  Profile
-]
+export default resolvers
