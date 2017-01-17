ede7871c785a5b01fb5f559d8f58dba1a59674a9
Gabriel Samson <gsamson@my.smccd.edu>
2017-01-17 11:02:58 -0800

    Adds skip picture + music uploader steps

--- src/workers/chat/fulfillments/onboarding/manual/index.js
+++ src/workers/chat/fulfillments/onboarding/manual/index.js
@@ -1,9 +1,13 @@
 import artistName from './artistName'
 import picture from './picture'
+import pictureLater from './pictureLater'
+import music from './music'
 
 const onboardingManualActionFunctions = {
   'onboarding.profileSetup.manual.artistName': artistName,
-  'onboarding.profileSetup.manual.picture': picture
+  'onboarding.profileSetup.manual.picture': picture,
+  'onboarding.profileSetup.manual.pictureLater': pictureLater,
+  'onboarding.profileSetup.manual.music': music
 }
 
 
