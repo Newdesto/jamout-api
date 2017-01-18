88c1714d67ae34753b165a56ade2fe5471766997
Chris Chuck <chrischuck35@gmail.com>
2017-01-17 22:02:29 -0800

    Fixes auth for studio resolver

--- src/resolvers/queries/studioEvent.js
+++ src/resolvers/queries/studioEvent.js
@@ -4,7 +4,7 @@ export default {
       throw new Error('Authentication failed.')
     }
 
-    if (user.roles.includes('partner:events')) {
+    if (user.roles.includes('partner:studio')) {
       return StudioEvent.fetchAll()
     }
 
