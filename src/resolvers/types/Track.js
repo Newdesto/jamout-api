e05675a27b3794ff722710b18053f497a137afa4
Chris Chuck <chrischuck35@gmail.com>
2017-01-23 11:23:28 -0800

    Removes gql schema in track resolver

--- src/resolvers/types/Track.js
+++ src/resolvers/types/Track.js
@@ -1,20 +1,6 @@
 import AWS from 'aws-sdk'
 const s3 = new AWS.S3()
 
-const Track = `
-  type Track {
-    id: ID!,
-    userId: ID!,
-    artworkKey: String,
-    audioKey: String!,
-    type: String!,
-    title: String!,
-    playCount: Int,
-    audioUrl: String,
-    artworkUrl: String
-  }
-`
-
 export const resolver = {
   audioUrl(track, args, context) {
     const params = { Bucket: 'jamout-music', Key: `${track.audioKey}` }
