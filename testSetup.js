36f5f5f56f6b9a4ac7f038669b65c9af8ca915a5
Gabriel Samson <gsamson@my.smccd.edu>
2017-01-23 17:06:33 -0800

    Catches a missing env config error in testSetup

--- testSetup.js
+++ testSetup.js
@@ -1,4 +1,8 @@
-require('app-module-path').addPath(__dirname + '/src')
+try {
+  require('app-module-path').addPath(__dirname + '/src')
+} catch (e) {
+  console.error(e)
+}
 require('dotenv').config()
 require('babel-register')
 require('babel-polyfill')
