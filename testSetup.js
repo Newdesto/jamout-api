0895a67420168442a5ca623da11c1850d68d4e82
Gabriel Samson <gsamson@my.smccd.edu>
2017-01-23 20:02:33 -0800

    Wraps env loader in try/catch

--- testSetup.js
+++ testSetup.js
@@ -1,7 +1,11 @@
-// If the node environment isn't CircleCI we assume that an env file exists.
-// Production bypasses env config loading.
-if (process.env.NODE_ENV !== 'test') {
+// We wrap .env loading in a try/catch because some environments, like
+// CircleCI don't use a .env file - instead they define it on the system level.
+// Production avoids this file entirely.
+try {
   require('dotenv').config()
+} catch (e) {
+  // Catch and surpress.
+  console.error(e)
 }
 require('app-module-path').addPath(__dirname + '/src')
 require('babel-register')
