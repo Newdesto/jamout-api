ecc49fa3d6a8d7f469ac49898d273ecaa38221cd
Gabriel Samson <gsamson@my.smccd.edu>
2017-01-23 17:38:58 -0800

    Fixes an import mix up in testSetup

--- testSetup.js
+++ testSetup.js
@@ -1,8 +1,8 @@
 // If the node environment isn't CircleCI we assume that an env file exists.
 // Production bypasses env config loading.
 if (process.env.NODE_ENV !== 'test') {
-  require('app-module-path').addPath(__dirname + '/src')
+  require('dotenv').config()
 }
-require('dotenv').config()
+require('app-module-path').addPath(__dirname + '/src')
 require('babel-register')
 require('babel-polyfill')
