54397dcf25e1e4489972a61934bc60dc49ef959d
Gabriel Samson <gsamson@my.smccd.edu>
2017-02-01 21:45:17 -0800

    Adds onboarding trigger

---
+++ src/workers/chat/actions/onboarding/trigger.js
@@ -0,0 +1,22 @@
+import { logger } from 'io'
+import { eventRequest } from 'io/apiai'
+import { handleAPIAIAction } from 'workers/chat/actions'
+
+export const trigger = async function trigger(message) {
+  logger.debug('Processing onboarding.trigger user action.')
+
+  // Query API.ai without any additional contexts.
+  const apiaiResponse = await eventRequest({
+    name: 'onboarding/welcome'
+  }, {
+    contexts: [
+      { name: 'authenticated' }
+    ],
+    sessionId: message.senderId
+  })
+
+  // Take the result and fulfill the action
+  await handleAPIAIAction(message, apiaiResponse.result)
+}
+
+export default trigger
