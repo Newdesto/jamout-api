4034acbd8abf13d27118b53405fffeeeb670d346
Chris Chuck <chrischuck35@gmail.com>
2017-02-03 00:12:33 -0800

    Fixes eslint errors

--- src/services/chat/postback/studio-sessions/newDate.js
+++ src/services/chat/postback/studio-sessions/newDate.js
@@ -2,9 +2,9 @@ import shortid from 'shortid'
 import microtime from 'microtime'
 import { publishMessages } from 'utils/chat'
 import { createJob } from 'io/queue'
-import format from 'date-fns/format'
-import Chat from 'services/chat'
-import StudioEvent from 'models/StudioEvent'
+// import format from 'date-fns/format'
+// import Chat from 'services/chat'
+// import StudioEvent from 'models/StudioEvent'
 
 const newDateHandler = async function newDateHandler({ user, channelId, values }) {
   // Create the response message
@@ -16,13 +16,12 @@ const newDateHandler = async function newDateHandler({ user, channelId, values }
     attachment: {
       type: 'StudioSessionNewDate',
       disableInput: false,
-      hideButtons: false,
+      hideButtons: false
     },
     visibleTo: values.visibleTo
   }
   await createJob('chat.persistMessage', { message: dateMessage })
   await publishMessages(channelId, user.id, [dateMessage])
-
 }
 
 export default newDateHandler
