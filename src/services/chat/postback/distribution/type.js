eb6cb159257d0567452b0742f150c7d1c4a886d8
Gabriel Samson <gsamson@my.smccd.edu>
2017-02-06 11:42:28 -0800

    Removes event message in favor of updating messages

--- src/services/chat/postback/distribution/type.js
+++ src/services/chat/postback/distribution/type.js
@@ -18,22 +18,7 @@ const dbReleaseEnum = {
   ALBUM: 'a'
 }
 
-/**
- * Publishes an event message and sends an inquiry message to the
- * studio's assistant channel.
- */
 const typeHandler = async function typeHandler({ user, channelId, values }) {
-  // Update the EditRelease.Type message to an Event message.
-  await Chat.updateMessage({
-    channelId,
-    timestamp: values.timestamp,
-    attachment: {
-      type: 'Event',
-      disableInput: false,
-      text: `You created a new ${readableReleaseEnum[values.type]} release.`
-    }
-  })
-
   // Create or update a release.
   let releaseId = values.releaseId
   if (releaseId) {
@@ -50,12 +35,12 @@ const typeHandler = async function typeHandler({ user, channelId, values }) {
     releaseId = release.id
   }
 
-  // By now we should for sure have a release. Post a Meta attachment message.
-  const metaMessage = {
+  // By now we should for sure have a release. Update the Type message so
+  // it's a Meta message.
+  // Update the EditRelease.Type message to an Event message.
+  await Chat.updateMessage({
     channelId,
-    id: shortid.generate(),
-    timestamp: microtime.nowDouble().toString(),
-    senderId: 'assistant',
+    timestamp: values.timestamp,
     attachment: {
       releaseId,
       subtype: 'Meta',
@@ -63,11 +48,7 @@ const typeHandler = async function typeHandler({ user, channelId, values }) {
       disableInput: true,
       postbackId: 'EditRelease.Meta'
     }
-  }
-
-  // Persist and publish the Meta message.
-  await createJob('chat.persistMessage', { message: metaMessage })
-  await publishMessages(channelId, 'assistant', [metaMessage])
+  })
 }
 
 export default typeHandler
