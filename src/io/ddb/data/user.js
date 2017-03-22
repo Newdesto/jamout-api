9bea8812776a50d5336f7de902b5ecd7c6b0e96b
Chris <chrischuck35@gmail.com>
2017-03-21 23:23:09 -0700

    Fixes typo in data

--- src/io/ddb/data/user.js
+++ src/io/ddb/data/user.js
@@ -16,9 +16,31 @@ export default [
     roles: [
       'artist',
       'partner:studio-circle-recordings',
-      'partner:deptive-media'
+      'partner:deptiv-media'
     ],
     updatedAt: '2017-01-23T08:45:31.656Z',
     username: 'gpsamson'
+  },
+  {
+    "email": "partner@123.com",
+    "username": "partner",
+    "displayName": "partner",
+    "permalink": "BJ6Uity3x",
+    "password": "$2a$10$SFyo92wgwyEgHsNr/QBFZeW1.nRE2WXR11DQIrh0u4NZ0Zf11vPne",
+    "id": "a93fd142-b480-44ea-9672-539f08b68380",
+    "createdAt": "2017-03-22T05:27:48.920Z",
+    "stripe": {
+      "customerId": "cus_AKhPBedSGaSkjL"
+    },
+    roles: [
+      'artist',
+      'partner:studio-circle-recordings',
+    ],
+    context: {
+      web: {
+        role: 'artist'
+      }
+    },
+    "updatedAt": "2017-03-22T05:27:49.248Z"
   }
 ]
