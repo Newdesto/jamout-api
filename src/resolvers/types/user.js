21fe8e27c49a36c9664b05e1adf06c28c2834221
Gabriel Samson <gsamson@my.smccd.edu>
2017-04-04 11:59:46 -0700

    Returns null for Avatar URL if an s3Key does not exist

--- src/resolvers/types/user.js
+++ src/resolvers/types/user.js
@@ -4,6 +4,10 @@ const s3 = new AWS.S3()
 
 const resolvers = {
   avatarUrl(user) {
+    if (!user.avatarKey) {
+      return null
+    }
+    
     const params = { Bucket: 'jamout-profile', Key: `${user.id}/avatar.png` }
     const url = s3.getSignedUrl('getObject', params)
     return url
