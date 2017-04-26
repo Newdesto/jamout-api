ca6fb19f0ff970e753fea920b29622427558e4a7
Chris <chrischuck35@gmail.com>
2017-04-26 14:05:02 -0700

    Adds test data

---
+++ src/io/ddb/data/track.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        "audioKey": "tracks/TheLetOut.mp3",
+        "createdAt": "2017-04-03T00:42:12.090Z",
+        "id": "10ca7f99-28f8-4a42-93be-0927e3709139",
+        "isPublic": true,
+        "playCount": 420,
+        "status": "done",
+        "title": "The Let Out",
+        "type": "original",
+        "updatedAt": "2017-04-03T00:42:13.559Z",
+        "user": {
+            "displayName": "chris",
+            "id": "693fd142-b480-44ea-9672-539f08b68389"
+        },
+        "userId": "693fd142-b480-44ea-9672-539f08b68389"
+    }
+]
