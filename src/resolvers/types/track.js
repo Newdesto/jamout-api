7d05c57eb297ebeb987c9bb9005384e001157e91
Gabriel Samson <gsamson@my.smccd.edu>
2017-04-30 22:09:30 -0700

    Changes hardcoded user to a GQL node

--- src/resolvers/types/track.js
+++ src/resolvers/types/track.js
@@ -3,6 +3,10 @@ import AWS from 'aws-sdk'
 const s3 = new AWS.S3()
 
 const resolvers = {
+  async user({ userId }, args, { User }) {
+    const user = await User.fetchById(userId)
+    return user
+  },
   audioUrl(track) {
     let params
     if (process.env.NODE_ENV !== 'production') {
