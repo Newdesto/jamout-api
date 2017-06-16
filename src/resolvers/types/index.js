426d4a305beb5f53630be65e1c3ad638470c925d
Gabriel Samson <gsamson@my.smccd.edu>
2017-06-16 12:03:24 -0700

    Fixes invalid resolver

--- src/resolvers/types/index.js
+++ src/resolvers/types/index.js
@@ -1,11 +1,9 @@
 import User from 'services/iam/types/user'
 import Node from './node'
 import Release from './release'
-import Track from './track'
 
 export default {
   Node,
   User,
   Release,
-  Track
 }
