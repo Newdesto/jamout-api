04fc6413aceb74da2183755b9e4b5bd741cd3364
Gabriel Samson <gsamson@my.smccd.edu>
2017-06-16 11:22:23 -0700

    Fixes merge conlficts

--- src/services/iam/mutations/index.js
+++ src/services/iam/mutations/index.js
@@ -2,23 +2,17 @@ import login from './login'
 import signUp from './signUp'
 import updateUser from './updateUser'
 import upgradeToPremium from './upgradeToPremium'
-<<<<<<< HEAD
 import verifyToken from './verifyToken'
+import updatePremium from './updatePremium'
+import cancelPremium from './cancelPremium'
 
 const mutations = {
   updateUser,
   login,
   verifyToken,
-  upgradeToPremium
-=======
-import updatePremium from './updatePremium'
-import cancelPremium from './cancelPremium'
-
-const mutations = {
   upgradeToPremium,
   updatePremium,
   cancelPremium
->>>>>>> 1ddca92453edaf5ac23a156a2e8988e8991d4100
 }
 
 export default mutations
