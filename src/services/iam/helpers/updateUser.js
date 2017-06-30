ecf9e96593849f2adbb9684053420f4a8da3414f
Gabriel Samson <gsamson@my.smccd.edu>
2017-06-29 21:49:11 -0700

    Removes dynamo merge

--- src/services/iam/helpers/updateUser.js
+++ src/services/iam/helpers/updateUser.js
@@ -2,7 +2,7 @@ import merge from 'dynamo-merge'
 import User from '../models/User'
 
 const updateUser = async function updateUser({ id, updates }) {
-  const { attrs } = await User.updateAsync({ id }, merge(updates))
+  const { attrs } = await User.updateAsync({ ...updates, id })
   return attrs
 }
 
