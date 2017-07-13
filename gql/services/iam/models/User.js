625bab2efba1c879b983844cff4adf598ac21589
Gabriel Samson <gsamson@my.smccd.edu>
2017-07-12 22:03:40 -0700

    Adds paid platforms channel to user type.

--- gql/services/iam/models/User.js
+++ gql/services/iam/models/User.js
@@ -24,6 +24,19 @@ const User = dynogels.define('User', {
     country: Joi.string(),
     blurb: Joi.string(),
 
+    channels: Joi.object().keys({
+      paidPlatforms: Joi.object().keys({
+        firstName: Joi.string(),
+        lastName: Joi.string(),
+        address: Joi.string(),
+        city: Joi.string(),
+        state: Joi.string(),
+        zipCode: Joi.string(),
+        phoneNumber: Joi.string(),
+        didAgree: Joi.boolean()
+      })
+    }),
+
     testMode: Joi.boolean().default(process.env.NODE_ENV !== 'production' ? true : undefined)
   },
   indexes: [
